6f6e56205d2e652dfddabca7818d52f50927335f
Morgan Rupard <17115504+mmrupard@users.noreply.github.com>
2024-08-16 11:05:23 -0700

    Added vec3 class and color header

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main()
@@ -14,15 +17,8 @@ int main()
 
         for (int i = 0; i < imageWidth; i++)
         {
-            auto r = double(i) / (imageWidth - 1);
-            auto g = double(j) / (imageHeight - 1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto pixel_color = color(double(i) / (imageWidth - 1), double(j) / (imageHeight - 1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
 
